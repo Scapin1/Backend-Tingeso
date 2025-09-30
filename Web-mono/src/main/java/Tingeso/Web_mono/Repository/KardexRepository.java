851af68c572d3951152411e1b1ccd809a4a02dd9
Lucciano Camilo Scapini yañez <lucciano.scapini@usach.cl>
2025-09-30 19:52:00 -0300

    Adds Kardex functionality
    Adds the Kardex feature, including a controller, service, and repository.
    
    This allows tracking of tool movements and history within the system.
    It includes methods for retrieving Kardex entries by tool ID and date range.

--- Web-mono/src/main/java/Tingeso/Web_mono/Repository/KardexRepository.java
+++ Web-mono/src/main/java/Tingeso/Web_mono/Repository/KardexRepository.java
@@ -2,8 +2,17 @@ package Tingeso.Web_mono.Repository;
 
 import Tingeso.Web_mono.Entity.KardexEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface KardexRepository extends JpaRepository<KardexEntity, Long> {
+
+    List<KardexEntity> findByToolId(Long toolId);
+
+    @Query("SELECT k FROM KardexEntity k WHERE k.movementDate BETWEEN :start AND :end")
+    List<KardexEntity> findBetweenDates(LocalDateTime start, LocalDateTime end);
 }
--- Web-mono/src/main/java/Tingeso/Web_mono/Service/KardexService.java
+++ Web-mono/src/main/java/Tingeso/Web_mono/Service/KardexService.java
@@ -1,7 +1,28 @@
 package Tingeso.Web_mono.Service;
 
+import Tingeso.Web_mono.Entity.KardexEntity;
+import Tingeso.Web_mono.Repository.KardexRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+
 @Service
+@RequiredArgsConstructor
 public class KardexService {
+    private final KardexRepository kardexRepository;
+
+    public List<KardexEntity> findToolKardex(Long toolId) {
+        return kardexRepository.findByToolId(toolId);
+    }
+
+    public List<KardexEntity> findBetweenDates(LocalDateTime start, LocalDateTime end) {
+        return kardexRepository.findBetweenDates(start, end);
+    }
+
+    public List<KardexEntity> getAll() {
+        return kardexRepository.findAll();
+    }
 }
